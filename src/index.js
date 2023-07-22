84c0031bc91f62ff22d8b2a88ebf36bf4a9a41b5
“eliltaA” <eliltatabrham@gmail.com>
2023-07-22 11:39:03 -0700

    cookies set

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import incomeObj from "./income.js";
+import expenseObj from "./expense.js";
+// const container = document.querySelector(".js-container");
+// const header = document.createElement("h1")
+// header.innerText = "hello world";
+// container.appendChild(header);
