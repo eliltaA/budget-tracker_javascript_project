3729ae6c551148c11b7e35f69b0c92a4505416b1
“eliltaA” <eliltatabrham@gmail.com>
2023-07-23 09:00:14 -0700

    bar chart

--- src/income.js
+++ src/income.js
@@ -1,3 +1,5 @@
+import Chart from 'chart.js/auto';
+
 class Income {
     constructor(){
         this.income = 0;
@@ -9,6 +11,9 @@ class Income {
         this.incomeAddForm = document.getElementById("incomeAddForm")
         this.incomeAddForm.addEventListener("submit", this.addIncomeForm.bind(this))
         this.loadFromCookies();
+        this.incomeData = []; // Initialize incomeData as an empty array
+        this.incomeLabels = [];
+        this.generateIncomeTable();
     }
 
      setCookie(name, value, daysToExpire) {
@@ -80,19 +85,116 @@ class Income {
         const newSourceInput = document.getElementById("newIncomeSource").value;
         const existingSource = document.getElementById("existingIncomeSource").value;
         const newSource = newSourceInput || existingSource
-    
+
+        if (newIncomeInput.trim() === '' || isNaN(parseFloat(newIncomeInput))) {
+            alert('Please enter a valid income amount.');
+            return;
+          }
         if (newIncomeInput !== ''){
             this.addIncome(newIncomeInput, newSource);
             this.updateSavings();
             this.saveToCookies();
+            this.generateIncomeTable();
         }
-        // location.reload();
+        //  location.reload();
     }
 
     getIncomeCategories() {
         const incomeCategories = this.getCookie("sources");
         return incomeCategories ? JSON.parse(incomeCategories) : [];
+    }
+
+    generateIncomeTable() {
+        const tableBody = document.getElementById("incomeTableBody");
+        tableBody.innerHTML = "";
+        
+        this.incomeData = []; // Clear the incomeData array
+        this.incomeLabels = [];
+        console.log(this.incomes);
+        this.incomes.forEach((income) => {
+          const newRow = document.createElement("tr");
+          newRow.innerHTML = `
+            <td>${income.source}</td>
+            <td>$${income.amount}</td>
+          `;
+          tableBody.appendChild(newRow);
+          this.incomeData.push(income.amount);
+          this.incomeLabels.push(income.source);
+        });
+        this.updateDoughnutChart(this.incomeLabels, this.incomeData);
+        this.updateBarChart(this.incomeLabels, this.incomeData)
       }
+    
+      // Add this function to your class to call it when you add a new income
+    updateIncomeTable() {
+        this.generateIncomeTable();
+    }  
+
+    updateDoughnutChart(labels, data) {
+        const incomeChart = document.getElementById("incomeChart").getContext("2d");
+      
+        if (this.incomeChartInstance) {
+            // If the chart already exists, destroy it first
+            this.incomeChartInstance.destroy();
+        }
+    
+        this.incomeChartInstance = new Chart(incomeChart, {
+            type: "doughnut",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        data: data,
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(255, 159, 64)',
+                            'rgb(255, 205, 86)',
+                            'rgb(75, 192, 192)',
+                            'rgb(54, 162, 235)',
+                            'rgb(1, 142, 203)',
+                            'rgb(106, 144, 204)',
+                            'rgb(1, 142, 203)',
+                            'rgb(102, 55, 221)',
+                        ],
+                    },
+                ],
+            },
+            options: {
+                // Add any custom options for the chart here
+            },
+        });
+    }
+    updateBarChart(labels, data) {
+        const incomeBarChart = document.getElementById("incomeBarChart").getContext("2d");
+
+        if (this.incomeBarChartInstance) {
+            // If the chart already exists, destroy it first
+            this.incomeBarChartInstance.destroy();
+        }
+
+        this.incomeBarChartInstance = new Chart(incomeBarChart, {
+            type: "bar",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Income Amount",
+                        data: data,
+                        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        });
+    }
 }
 
 const incomeObj = new Income()
