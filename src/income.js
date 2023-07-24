e608ffb7a559c8163e194e7c29919c7f8aa2f587
“eliltaA” <eliltatabrham@gmail.com>
2023-07-24 16:38:05 -0700

    Pages Done

--- src/income.js
+++ src/income.js
@@ -5,6 +5,7 @@ class Income {
         this.income = 0;
         this.incomes = [];
         this.budget = 0;
+        this.savings = 0;
         this.incomeCategories = this.getIncomeCategories();
         this.budgetCategories = [];
         this.budgetAddBtn = document.getElementById("budgetAddbtn")
@@ -18,6 +19,7 @@ class Income {
         this.updateBudgetTotal();
         this.updateSavings();
         this.generateBudgetTable();
+        this.generateBarChart();
     }
 
      setCookie(name, value, daysToExpire) {
@@ -282,7 +284,45 @@ class Income {
         document.getElementById('incomeBudgetValue').textContent = this.budget;
       }
     
-    
+      generateBarChart() {
+        const incomeVsBudgetChart = document.getElementById("incomeVsBudgetChart").getContext("2d");
+
+        if (this.incomeVsBudgetChartInstance) {
+            // If the chart already exists, destroy it first
+            this.incomeVsBudgetChartInstance.destroy();
+        }
+
+        this.incomeVsBudgetChartInstance = new Chart(incomeVsBudgetChart, {
+            type: "bar",
+            data: {
+                labels: ["Income", "Budget", "Savings"],
+                datasets: [
+                    {
+                        label: "Amount",
+                        data: [this.income, this.budget, this.savings],
+                        backgroundColor: [
+                            "rgba(255, 99, 132, 0.6)",
+                            "rgba(54, 162, 235, 0.6)",
+                            "rgba(75, 192, 192, 0.6)",
+                        ],
+                        borderColor: [
+                            "rgba(255, 99, 132, 1)",
+                            "rgba(54, 162, 235, 1)",
+                            "rgba(75, 192, 192, 1)",
+                        ],
+                        borderWidth: 1,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        });
+    }
     
     initiate() {
         //document.getElementById('incomeValue').textContent = this.income;
